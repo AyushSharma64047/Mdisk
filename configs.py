7b4c1c6d01de965269820d95ffa906fff57374fa
Aayush64047Developer <106085120+AyushSharma64047@users.noreply.github.com>
2022-10-14 00:41:43 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,18 +1,18 @@
-# (c) @RoyalKrrishna
+# (c) @pankajpandiyar
 
 import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", "12336818"))
-    API_HASH = os.environ.get("API_HASH", "de4c34807c8963ba9418e01c7cc15c4c")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5462111014:AAGuKNZq6Xc_Xl9oTXLST3k0inOjEo_aNl4")
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchBot")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQAA0zsXR8oiLOX3oseq-EBvLRFU9PaF-LeMzN3gWUvGOs4R_hlxB2p98o6Gkf9rlbtF5OIItP1CnZKA34HiSLDtc5-d-M8p69DvFwz-BdQsVb3g_NteltcOZxAqtyTriTlAfVgNyb66P2sVdLUhu2ow0BfCmNDEcXHcWs2MVvexjY2Emxuvs73ED_QvEAg8Vt1IbGxWM2PIpBTbP2VTELuYoYYdadlfUY4WeOy9cGB4DXAG6TkPdClvRafuOOgmC6rS24iUYct2wDLsqYmOydJhYsX74mVOKW7C2mmpH184iAsbiJWg3YTmnTa-DIqiG0DpElZLLXiiHiKuCzeP91ACAAAAAUQdfasA")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001778120240"))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Mdisk_Links_Sender_Bot")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER","5437750699"))
-    FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "-1001557431626")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", -100)
     ABOUT_BOT_TEXT = """<b>This is Mdisk Search Bot.
 
 🤖 My Name: <a href='https://t.me/MdiskLinksSearchBot'>Mdisk Search Robo</a>
